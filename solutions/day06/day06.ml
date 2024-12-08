677d1c2143173a9b6303029e1c3790cc5f4076fe
Bob Peterson <bob@rwp.is>
2024-12-07 23:37:07 -0600

    day 6 part 2

--- solutions/day06/day06.ml
+++ solutions/day06/day06.ml
@@ -10,7 +10,7 @@ let next_direction = function
   | W -> N
 
 type position = {
-  visits: int;
+  visits: int option;
   guard: direction option;
 }
 
@@ -18,15 +18,16 @@ type map = {
   m: (position, unit) t array array;
   mutable i: int;
   mutable j: int;
+  mutable max_rpts: (int * int) * int;
 }
 
 let parse_start = function
   | '#' -> Right ()
-  | '.' -> Left { visits = 0; guard = None }
-  | '^' -> Left { visits = 0; guard = Some N }
-  | 'v' -> Left { visits = 0; guard = Some S }
-  | '>' -> Left { visits = 0; guard = Some E }
-  | '<' -> Left { visits = 0; guard = Some W }
+  | '.' -> Left { visits = None; guard = None }
+  | '^' -> Left { visits = None; guard = Some N }
+  | 'v' -> Left { visits = None; guard = Some S }
+  | '>' -> Left { visits = None; guard = Some E }
+  | '<' -> Left { visits = None; guard = Some W }
   | _ -> failwith "unknown starting state"
 
 let pos_has_guard = function
@@ -50,7 +51,7 @@ let make_map s =
   let i = match Array.find_index pos_has_guard m.(j) with
   | Some ii -> ii
   | None -> failwith "has to have a starting column" in
-  { m = m; i = i; j = j }
+  { m = m; i = i; j = j; max_rpts = ((0, 0), 0) }
 
 let turn_right m =
   match m.m.(m.j).(m.i) with
@@ -60,16 +61,27 @@ let turn_right m =
     m.m.(m.j).(m.i) <- Left { visits = v; guard = Some (next_direction d) }
     end
 
+exception Cyclic
+
 let go_there m i j k l =
   match m.m.(m.j).(m.i) with
   | Right () -> failwith "can't be inside an obstruction"
   | Left { visits = _; guard = None } -> failwith "can't not be a guard where guard is"
   | Left { visits = v; guard = Some d } -> begin
-    m.m.(j).(i) <- Left { visits = v + 1; guard = None };
+    m.m.(j).(i) <- Left { visits = Some ((try Option.get v with _ -> 0) + 1); guard = None };
     m.m.(l).(k) <- begin
       let v0 = match m.m.(l).(k) with
         | Right () -> failwith "same same"
-        | Left { visits = vv; guard = _ } -> vv in
+        | Left { visits = w; guard = _ } -> w in
+      begin match v0 with
+      | Some u -> begin
+        if u > snd m.max_rpts then
+          m.max_rpts <- ((k, l), u);
+          if u > 8 then (* arb threshold or better constraint? *)
+          raise Cyclic
+        end
+      | None -> ()
+      end;
       Left { visits = v0; guard = Some d }
     end;
     m.i <- k;
@@ -106,7 +118,8 @@ let rec run_route m =
 let count_spot p tot =
   match p with
   | Right () -> ()
-  | Left { visits = v; guard = _ } -> if v > 0 then tot := !tot + 1
+  | Left { visits = None; guard = _ } -> ()
+  | Left { visits = Some _; guard = _ } -> tot := !tot + 1
 
 let count_row r tot =
   Array.iter (fun p -> count_spot p tot) r
@@ -123,11 +136,30 @@ let part1 input =
   count_pos m
 
 let part2 input =
-  let _ = input in
-  0
+  let s = Utils.read_file input in
+  let m' = make_map s in
+  let x = Array.length m'.m.(0) in
+  let y = Array.length m'.m - 1 in
+  Printf.printf "x = %d, y = %d\n" x y;
+  let num_cyclic = ref 0 in
+  for ii = 0 to x - 1 do
+    for jj = 0 to y - 1 do
+      let m = make_map s in
+      match m.m.(jj).(ii) with
+      | Right _ | Left { visits = _; guard = None } -> begin
+        m.m.(jj).(ii) <- Right ();
+        match run_route m with
+        | _ -> () (* left the board; not cyclic *)
+        | exception Cyclic -> num_cyclic := !num_cyclic + 1;
+        | exception Invalid_argument a -> Printf.printf "LMAO: %s\n" a;
+        end
+      | _ -> () (* don't place obstruction on top of guard starting position *)
+    done
+  done;
+  !num_cyclic
 
 let solve filename =
   let p1 = part1 filename in
-  let p2 = part2 filename in
+  let p2 = part2 filename in (* u > 2: 1790 too high, u > 4: 1767  *)
   Printf.printf "Part 1: %d\n" p1;
   Printf.printf "Part 2: %d\n" p2
